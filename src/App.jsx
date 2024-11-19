0ee9aa9d3e510228cfe4fb3837004607e4b2fbd0
Lali Bibilashvili <lalibibilashvili@gmail.com>
2024-11-19 23:45:05 +0100

    exercise 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   let tutorialData = [
@@ -8,22 +9,22 @@ function App() {
       title: "Dedica moltes hores",
       description:
         "Un minim de 30 hores a la setmana. Si no en tens prou, hauras de dedicar-li, mes hores. Al principi sembla impossible pero una millora rapidament",
-      bgColor: "myColor",
-      image: "myImg",
+      bgColor: "#45818e",
+      image: "assets/time_management.svg",
     },
     {
-      title: "programa prorjects propis",
+      title: "Programa prorjects propis",
       description:
         "Mes val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivacio i la implicacio en el projecte ajudara, a accelerar el teu aprentatge.",
-      bgColor: "",
-      image: "",
+      bgColor: "#bcbcbc",
+      image: "assets/programming.svg",
     },
     {
       title: "Procura descansar",
       description:
         "Descansar be i desconnectar son vitals. D aquesta manera reduiras l'éstres i l'ansietat. Miloraras la teva concentracio i consolidaras el teu aprenentatge.  ",
-      bgColor: "",
-      image: "",
+      bgColor: "#f1c232",
+      image: "assets/meditation.svg",
     },
   ];
 
@@ -37,7 +38,7 @@ function App() {
   console.log(currentCardData);
   return (
     <>
-      <Card data={currentCardData} nextPage={nextStep} />
+      <Card data={currentCardData} nextPage={nextStep} step={step} />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,34 @@
 /* eslint-disable react/prop-types */
-function Card({ data, nextPage }) {
+
+function Card({ data, nextPage, step }) {
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
-      <button onClick={nextPage}>click</button>
+    <div className="card " style={{ width: "18rem" }}>
+      <img
+        src={data.image}
+        style={{
+          backgroundColor: data.bgColor,
+        }}
+        className="card-img-top"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{data.title}</h5>
+        <p className="card-text">{data.description}</p>
+      </div>
+      <div className="container d-flex justify-content-between p-4">
+        <div className="nav-group d-flex justify-content-evenly">
+          <span className="fs-1 rounded">{step === 0 ? "-" : "·"}</span>
+          <span className="fs-1 rounded">{step === 1 ? "-" : "·"}</span>
+          <span className="fs-1 rounded">{step === 2 ? "-" : "·"}</span>
+        </div>
+
+        <button
+          className="btn btn-dark rounded-circle"
+          onClick={nextPage}
+          style={{ width: "50px", height: "50px" }}
+        >
+          &#8594;
+        </button>
+      </div>
     </div>
   );
 }
